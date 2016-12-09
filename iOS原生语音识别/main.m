f7b50fcafdf59fbe5b576269e605de7bef2ff9f5
尹啟星 <yinxing_cb@163.com>
2016-12-09 23:17:12 +0800

    init

---
+++ iOS原生语音识别/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS原生语音识别
+//
+//  Created by 尹啟星 on 2016/12/9.
+//  Copyright © 2016年 尹啟星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
